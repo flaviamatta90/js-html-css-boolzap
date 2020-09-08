8f4d92e61cf9b7edf9aea0f837ecdab500b66941
Flavia <flaviamatta90@gmail.com>
2020-09-08 20:06:59 +0200

    jquery

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@
 $(document).ready(
   function (){
 
+    $("#send").keyup(
+      function(event){
+        if(event.which == 13){
+          var inputValue = $("#send").val();
 
-
-
-
-
-
-
-
-
-
+          if(inputValue != ""){
+            var elemento = $(".template  h4").clone();
+            elemento.prepend(inputValue);
+            $(".chat-none").append(elemento);
+            $("#send").val("");
+          }
+        }
+      }
+    );
 
 
   }
