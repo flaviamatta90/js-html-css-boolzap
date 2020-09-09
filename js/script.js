4dd4f152708ceff5a1577b418542c95cb9e42e10
Flavia <flaviamatta90@gmail.com>
2020-09-09 18:30:49 +0200

    with a random words

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(
   function (){
 
-
+// funzioni generali
 $(".sent-message").click(
   function(){
     sentMessage();
@@ -17,9 +17,17 @@ $("#sent").keyup(
   }
 );
 
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 var d = new Date();
 hour = d.getHours();
 min = d.getMinutes();
+
+// /funzioni generali
+
+// inviare un messaggio inserendolo nel cloud opportuno
 
 function sentMessage(){
   var inputText = $("#sent").val();
@@ -27,29 +35,30 @@ function sentMessage(){
   if(inputText != ""){
     var templateMessage = $(".template .inside-chat").clone();
 
+// cloud verde
     templateMessage.find(".messaggio").text(inputText);
     templateMessage.find(".orario").text(hour + ":" + min);
     templateMessage.addClass("inviato");
 
     $(".chat-side").append(templateMessage);
     $("#sent").val("");
-  }
 
+// cloud risposta ogni secondo
   setTimeout(function(){
-    var bot = ["ciao", "bene"];
-
+    var bot = ["Ciao", "bene", "E tu?", "Che mi dici?", "Son content* per te!", "Gni"," Uffa", "Bleehh"];
     var ricevuto = $(".template .inside-chat").clone();
 
-    ricevuto.find(".messaggio").text(bot[1]);
+    ricevuto.find(".messaggio").text(bot[numeroRandom(0, 8)]);
     ricevuto.find(".orario").text(hour + ":" + min);
     $(".chat-side").append(ricevuto);
 
     }, 1000);
 
-}
+  }
+};
 
 
 
-
+// fine
   }
 );
